b3dc0a87e0e3e8f29ed13eb3d87028f7381b6e37
Savwede Godswill <godswillsavwede@gmail.com>
2022-09-09 20:18:42 +0100

    reverse a-z

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ * C - Variables, if, else, while
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char let;
+
+	let =  'z';
+	while (let >= 'a')
+	{
+		putchar(let);
+		let++;
+	}
+	putchar('\n');
+	return (0);
+}
