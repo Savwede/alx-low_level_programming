c89e4012753dee0b9241226d84dda4e3d98a15b8
Savwede Godswill <godswillsavwede@gmail.com>
2022-09-09 11:15:51 +0100

    correction for 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,7 +9,7 @@
 
 int main(void)
 {
-char str[] = "$(echo \"and that piece of art is useful\" - Dora Korpar, 2015-10-19\")" 
+char str[] = "$(echo \"and that piece of art is useful\" - Dora Korpar, 2015-10-19\")"; 
 /**/
 system(str);
  return (0);
