a0b5c1eee3cba397d16309478b7a0c9bd462f86c
Savwede Godswill <godswillsavwede@gmail.com>
2022-09-09 11:12:02 +0100

    added 100-intel and 101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ * Printing directly to the system instesd of printf or puts
+ */
+
+int main(void)
+{
+char str[] = "$(echo \"and that piece of art is useful\" - Dora Korpar, 2015-10-19\")" 
+/**/
+system(str);
+ return (0);
+}
