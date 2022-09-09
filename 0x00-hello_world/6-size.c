d1d755f6c0f1395fa2536b83731390da1a9394d8
Savwede Godswill <godswillsavwede@gmail.com>
2022-09-09 10:19:10 +0100

    added 6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+/* printing the size of the primitive data types */
+printf("Size of a char: %d byte(s)\n",char);
+printf("Size of an int: %d byte(s)\n",int);
+printf("Size of a long int: %d byte(s)\n", long int);
+printf("Size of a long long int: %d byte(s)\n",long long int);
+printf("Size of a float: %d byte(s)\n", float);
+return (0);
+}
