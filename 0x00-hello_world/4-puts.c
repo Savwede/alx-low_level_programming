3ce581dcd6b8ff3613c13f86606d42f73966328f
Savwede <111609631+Savwede@users.noreply.github.com>
2022-09-09 09:33:44 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -10,7 +10,7 @@
 int main(void)
 {
 char str[] = "\"Programming is like building a multilingual puzzle";
-/* running the put statement */
+/* running the put statement*/
 puts(str); 
-return(0);
+return (0);
 }
