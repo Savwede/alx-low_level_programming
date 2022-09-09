7ed5888b110433b08b79e0e8364e17715fd32f53
Savwede Godswill <godswillsavwede@gmail.com>
2022-09-09 09:42:55 +0100

    added 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+/* running the put statement*/
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
