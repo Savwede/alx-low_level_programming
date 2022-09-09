ff838fc41ec549aea3a5eab2351b871045733f1c
Savwede Godswill <godswillsavwede@gmail.com>
2022-09-09 09:16:36 +0100

    updated 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,10 +7,9 @@
  * Return: Always 0 (Success)
  */
 
-int main() {
-
-  char str[] = "Programming is like building a multilingual puzzle";
-  puts(str);
-
-  return 0;
+int main(void)
+{
+      char str[] = "\"Programming is like building a multilingual puzzle";
+      puts(str);
+      return (0);
 }
