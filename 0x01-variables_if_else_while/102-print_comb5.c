87140eb5ddb9c9db47ee84146fb03555a8796eb2
Savwede Godswill Eguolor <69734725+johnson-wills@users.noreply.github.com>
2022-09-11 06:58:07 +0100

    Update 102-print_comb5.c
    Using just two for loops instead of the 4 while loops

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,45 +11,27 @@
  */
 int main(void)
 {
-	int i;
-	int j;
-	int s;
-	int v;
-
-	i = 0;
-	while (i < 10)
-	{
-		j = 0;
-		while (j < 10)
-		{
-			v = 0;
-			while (v < 10)
-			{
-				s = 0;
-				while (s < 10)
-				{
-					if ((v + s > i + j) && v >= i && i >= j)
-					{
-						putchar(i + '0');
-						putchar(j + '0');
-						putchar(' ');
-						putchar(v + '0');
-						putchar(s + '0');
-						if (j < 8 || i < 9)
-						{
-							putchar(',');
-							putchar(' ');
-						}
-					}
-					s++;
-				}
-				v++;
-			}
-			j++;
-		}
-		i++;
-	}
-	putchar('\n');
-
-	return (0);
+     int i, j;
+ 
+     for (i = 0; i <= 98; i++)
+     {
+         for (j = i + 1; j <= 99; j++)
+         {
+             putchar((i / 10) + '0');
+             putchar((i % 10) + '0');
+             putchar(' ');
+             putchar((j / 10) + '0');
+             putchar((j % 10) + '0');
+ 
+             if (i == 98 && j == 99)
+                 continue;
+ 
+             putchar(',');
+             putchar(' ');
+         }
+     }
+ 
+     putchar('\n');
+ 
+     return (0);
 }
