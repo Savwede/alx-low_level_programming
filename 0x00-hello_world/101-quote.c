6b9abe4f6ada9d6eb7ddf4099ef5cf1ad595cb94
Savwede <111609631+Savwede@users.noreply.github.com>
2022-09-12 13:40:46 +0100

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,14 +3,14 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: 1  (error)
  * Printing directly to the system instesd of printf or puts
  */
 
 int main(void)
 {
-char str[] = "$(echo \"and that piece of art is useful\" - Dora Korpar, 2015-10-19\")"; 
 /**/
-system(str);
- return (0);
+fprintf(stderr, "and that piece of art is useful\" \
+- Dora Korpar, 2015-10-19\n");
+ return (1);
 }
