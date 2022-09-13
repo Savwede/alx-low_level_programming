5164605e45f147a13bd1d70aac18f30ee67fc516
Savwede Godswill <godswillsavwede@gmail.com>
2022-09-13 19:38:31 +0100

    added REAME.md and 0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int main(void)
+{
+	char ch[] = "_putchar";
+	int i;
+
+	for (i = 0; i < 8; i++)
+	{
+		putchar(ch[i]);
+	}
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,5 @@
+every function in this project only uses
+# _putchar
+or
+# system_call
+to write to standard output. no standard library is allowed.
