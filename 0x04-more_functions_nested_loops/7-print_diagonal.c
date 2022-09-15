921eb36890014267dfa00100aaae709795d7b57a
Savwede Godswill <godswillsavwede@gmail.com>
2022-09-15 21:54:10 +0100

    ..

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,4 +1,5 @@
 #include "main.h"
+void spaces(int n);
 
 /**
  * print_diagonal - draws a diagonal line in the terminal
@@ -13,9 +14,26 @@ void print_diagonal(int n)
 
 	for (i = 0; i < n; i++)
 	{
-		print_char_ntimes(' ', i);
+		spaces(i);
 		_putchar('\\');
 		_putchar('\n');
 	}
 	_putchar('\n');
+}
+
+/**
+ * spaces - prints space in the terminal
+ * @n: is the number of times the space character should be printed
+ *
+ * Return: void.
+ */
+
+void spaces(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar(' ');
+	}
 }
