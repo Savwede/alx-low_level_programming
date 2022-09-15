759d4ef063986b67c545fae621443bec27a2a232
Savwede Godswill <godswillsavwede@gmail.com>
2022-09-15 09:16:05 +0100

    files for debugging task

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - test the function positive_or_negative which should
+ * prints either a number is -ve, 0 or +ve
+ * Return: Always 0 (Success)
+ */
+
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,3 @@
+the files in this directory are simply corrections to
+some code that were not working
+note: this directory does not contain the broken codes
