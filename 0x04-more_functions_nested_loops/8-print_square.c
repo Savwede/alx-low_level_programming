2f8fd27a73d8955f803fce02dce2f5aa0a3ef183
Savwede Godswill <godswillsavwede@gmail.com>
2022-09-15 22:18:25 +0100

    ...

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,4 +1,22 @@
 #include "main.h"
+/**
+ * print_char_ntimes - prints a given character,
+ * a given number of times to the terminal
+ * @c: the character to be printed
+ * @n: is the number of times the space character should be printed
+ *
+ * Return: void.
+ */
+
+void print_char_ntimes(char c, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar(c);
+	}
+}
 
 /**
  * print_square - draws a square in the terminal
