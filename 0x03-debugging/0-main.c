8f6eb757ad51c1777edd90c4dc499be8009f8a45
Savwede Godswill <godswillsavwede@gmail.com>
2022-09-15 09:27:48 +0100

    ...

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,11 +1,9 @@
 #include "main.h"
-
 /**
  * main - test the function positive_or_negative which should
  * prints either a number is -ve, 0 or +ve
  * Return: Always 0 (Success)
  */
-
 
 int main(void)
 {
