7943268de16dfb6d6d3ba57df0d7d5c643c67dc2
Wills <godswillsavwede@gmail.com>
2022-12-09 13:50:01 +0100

    ...

--- 0x17-doubly_linked_lists/test/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/test/7-insert_dnodeint.c
@@ -12,13 +12,13 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	unsigned int cnt = 0;
-	dlistint_t *new_node, *nest_node, *tmp = *h;
+	dlistint_t *new_node, *tmp = *h;
 
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 		return (NULL);
 
-	while (cnt < idx - 1)
+	while (cnt < (idx - 1))
 	{
 
 		cnt++;
@@ -27,11 +27,9 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 		tmp = tmp->next;
 	}
-	nest_node = tmp->next;
-	nest_node->prev = new_node;
-	tmp->next = new_node;
 	new_node->prev = tmp;
-	new_node->next = nest_node;
+	new_node->next = tmp->next;
+	tmp->next = new_node;
 	new_node->n = n;
 	return (new_node);
 }
