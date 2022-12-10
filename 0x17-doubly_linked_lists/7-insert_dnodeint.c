2c585877c35b11f7b6bb3d34f92c88f5dc34b559
Savwede <111609631+Savwede@users.noreply.github.com>
2022-12-10 11:35:45 +0100

    Update 7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -22,10 +22,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	while (cnt < idx)
 	{
 		if (tmp == NULL)
-		{
-			printf("null encountered in loop\n");
 			return (NULL);
-		}
 
 		cnt++;
 		tmp = tmp->next;
