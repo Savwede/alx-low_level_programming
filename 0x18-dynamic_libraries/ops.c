7bf54bee45429875d5cbbb66a77735daf82f59c4
Savwede Godswill <godswillsavwede@gmail.com>
2022-12-13 07:53:21 +0100

    added ops.c and created 100-operations.so

---
+++ 0x18-dynamic_libraries/ops.c
@@ -0,0 +1,58 @@
+/**
+ * add - adds two ints
+ * @a: first int
+ * @b: second int
+ *
+ * Return: the sum of a and b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - finds the differece of two ints
+ * @a: first int
+ * @b: second int
+ *
+ * Return: the differece of a and b
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - multiplies two ints
+ * @a: first int
+ * @b: second int
+ *
+ * Return: the product of a and b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * div - divides two ints
+ * @a: first int
+ * @b: second int
+ *
+ * Return: the quotient of a and b
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - finds the b modulus of a
+ * @a: first int
+ * @b: second int
+ *
+ * Return: a mod b
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
